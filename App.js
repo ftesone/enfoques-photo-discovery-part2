f76eb074ae03121a096fad1be010e0f44e0fceb3
Fernando Tesone <fenatesone@gmail.com>
2023-10-17 22:27:33 -0300

    Add redux with async storage

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Discover } from './screens/Discover';
@@ -8,12 +11,16 @@ const Stack = createNativeStackNavigator();
 
 export function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Discover">
-        <Stack.Screen name="Discover" component={Discover} options={{ headerShown: false }} />
-        <Stack.Screen name="Favs" component={Favs} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Discover">
+            <Stack.Screen name="Discover" component={Discover} options={{ headerShown: false }} />
+            <Stack.Screen name="Favs" component={Favs} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
 
